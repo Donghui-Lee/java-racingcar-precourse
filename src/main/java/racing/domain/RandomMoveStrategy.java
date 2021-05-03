00ffbe352c058d279563fe4effd93aba2cc5e8b1
Donghui-Lee <ehdgml3206@naver.com>
2021-05-03 20:33:29 +0900

    feat: 랜덤 움직임 전략 패턴 구현

---
+++ src/main/java/racing/domain/RandomMoveStrategy.java
@@ -0,0 +1,15 @@
+package racing.domain;
+
+import java.util.Random;
+
+public class RandomMoveStrategy implements MoveStrategy{
+    private static final int RANDOM_RANGE = 10;
+    private static final int MOVE_RESTRICT = 4;
+
+    private final Random random = new Random();
+
+    @Override
+    public boolean movable() {
+        return random.nextInt(RANDOM_RANGE) >= MOVE_RESTRICT;
+    }
+}
