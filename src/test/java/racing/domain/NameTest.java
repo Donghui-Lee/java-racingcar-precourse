05468b2816a9653f2c39d6883958627a552f4703
Donghui-Lee <ehdgml3206@naver.com>
2021-05-03 19:17:58 +0900

    feat: 이름 정보 Name Class 추가

---
+++ src/test/java/racing/domain/NameTest.java
@@ -0,0 +1,27 @@
+package racing.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+
+    @Test
+    void 생성_테스트() {
+        // given
+        Name name = Name.of("ldh");
+        // when & then
+        assertThat(name).isEqualTo(Name.of("ldh"));
+    }
+
+    @Test
+    void 글자수_초과_예외_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Name.of("123456"));
+    }
+
+    @Test
+    void 빈값_예외_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Name.of(""));
+    }
+}
